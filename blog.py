c0addf1d4f6905a2e90e66d7e6a69464170f55e0
rails new my_shopify_app <viotrip@gmail.com>
2022-04-15 01:51:55 -0400

    main

--- blog.py
+++ blog.py
@@ -38,7 +38,7 @@ def blogSectionExpander(prompt1):
       engine="davinci-instruct-beta-v3",
       prompt="Expand the blog section in to a detailed professional , witty and clever explanation.\n\n {}".format(prompt1),
       temperature=0.7,
-      max_tokens=300,
+      max_tokens=800,
       top_p=1,
       frequency_penalty=0,
       presence_penalty=0
